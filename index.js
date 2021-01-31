5d681f60a941014a0188b58640a9a763761dfbf3
İsmail Deniz Tanisman <kudusever01@gmail.com>
2021-01-31 09:13:26 +0300

    başlıyoruz

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const rp = require('request-promise');
+// denemem
 const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 const express = require('express');
